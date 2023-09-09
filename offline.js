977dba74a4c94a0a0fd2390325186a9a040ffea3
Nekonyna <nawaphat321@gmail.com>
2023-09-09 14:40:57 +0700

    test4

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1694104909,
+	"version": 1694245166,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -44,6 +44,7 @@
 		"images/sprite30-sheet0.png",
 		"images/sprite33-sheet0.png",
 		"images/sprite35-sheet0.png",
+		"images/fr-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
